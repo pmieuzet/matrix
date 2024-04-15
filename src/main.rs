2aa7bc526983b5fd98cf85ec274496e8af05f0f8
pmieuzet <pauline.mieuzet@gmail.com>
2024-04-15 17:33:27 +0200

    impl size

--- src/main.rs
+++ src/main.rs
@@ -2,14 +2,22 @@ use std::fmt::Display;
 
 #[derive(Clone, Debug)]
 struct Vector<T> {
+    size: usize,
     data: Vec<T>,
 }
 impl<T> Display for Vector<T> {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {}
 }
+impl<T> Vector<T> {
+    fn size(&self) -> usize {
+        self.size
+    }
+}
 
 #[derive(Clone, Debug)]
 struct Matrix<T> {
+    rows: usize,
+    colums: usize,
     data: Vec<Vec<T>>,
 }
 impl<T> Display for Matrix<T> {
