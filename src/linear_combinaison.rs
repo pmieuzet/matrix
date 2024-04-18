6c2972c0e1075d0adc90ba3ce3765e1b36c8f81e
pmieuzet <pauline.mieuzet@gmail.com>
2024-04-18 08:09:41 +0200

    minor changes

--- src/linear_combinaison.rs
+++ src/linear_combinaison.rs
@@ -2,6 +2,7 @@ use std::ops::{Add, Mul};
 
 use crate::{errors::Error, vector::Vector};
 
+/// Creation of a new vector by multiplying each vector by a corresponding scalar, then adding the results
 pub fn linear_combination<K>(u: &[Vector<K>], coefs: &[K]) -> Result<Vector<K>, Error>
 where
     K: std::ops::AddAssign + Clone + std::ops::Mul<Output = K> + std::ops::Add<Output = K>,
